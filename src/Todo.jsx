913aa83b52e0ee48ac5c09a07778743330759700
Ruba24 <rubaqazi2000@gmail.com>
2024-10-15 23:07:50 +0500

    Added Nice CSS

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -29,33 +29,44 @@
 // export default ToDo
 
 import React, { useState } from "react";
-function ToDo(){
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState("");
-    const addTask = (e) => {
-        e.preventDefault();
-        setTasks([...tasks, newTask]);
-        setNewTask('');
-    }
-    const deleteTask = (index) => {
-        setTasks(tasks.filter((task, i) => i !== index));
-      };
-    
-    return(
-        <>
-        <h1>Todo App Using React</h1>
-        <input type="text" name="" id="" placeholder="Enter New Task Here" value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
-        <button onClick={addTask}>Add</button>
-        <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
-            <button onClick={() => deleteTask(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+import "./app.css";
+function ToDo() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const addTask = (e) => {
+    e.preventDefault();
+    setTasks([...tasks, newTask]);
+    setNewTask("");
+  };
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((task, i) => i !== index));
+  };
 
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <h1>Todo App Using React</h1>
+        <div className="todo-body">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter New Task Here"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+        </div>
+        <button id="addbtn" onClick={addTask}>Add</button>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <button id="deletebtn" onClick={() => deleteTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
-export default ToDo
+export default ToDo;
